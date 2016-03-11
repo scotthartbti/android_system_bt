d54251d679c947fed10eb6a678ed4fbfae8f4462
scott <scott@ubuntu>
2016-03-11 12:16:46 -0500

    another merge derp
    Change-Id: I26eb5844b5ac5cb49cae657975b9629e20cfba0c
    Signed-off-by: scott <scott@ubuntu>

--- btif/src/btif_config.c
+++ btif/src/btif_config.c
@@ -360,10 +360,9 @@ void btif_config_flush(void) {
   assert(alarm_timer != NULL);
 
   alarm_cancel(alarm_timer);
-  btif_config_write(0, NULL);
 
   config_flush(CONFIG_FILE_PATH);
-  btif_config_write();
+  btif_config_write(0, NULL);
 }
 
 int btif_config_clear(void){
